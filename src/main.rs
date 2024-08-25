491a3c976043c0a3e8da6a263568b5c16429eb9b
Steven Ferrer <steven.r.ferrer@gmail.com>
2024-08-25 15:34:51 +0800

    Added system and runtime

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,43 @@
-mod balances;
+use rsm::{balances, system};
+
+// This is our main Runtime.
+// It accumulates all of the different pallets we want to use.
+#[derive(Debug)]
+pub struct Runtime {
+	system: system::Pallet,
+	balances: balances::Pallet,
+}
+
+impl Runtime {
+	// Create a new instance of the main Runtime, by creating a new instance of each pallet.
+	fn new() -> Self {
+		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+	}
+}
 
 fn main() {
-	println!("Hello, world!");
+	let mut rt = Runtime::new();
+
+	let alice = "alice".to_string();
+	let bob = "bob".to_string();
+	let charlie = "charlie".to_string();
+
+	rt.balances.set_balance(&alice, 100);
+
+	rt.system.inc_block_number();
+	assert_eq!(rt.system.block_number(), 1);
+
+	rt.system.inc_nonce(&alice);
+	let _res = rt
+		.balances
+		.transfer(alice.clone(), bob.clone(), 30)
+		.map_err(|e| eprintln!("error: {}", e));
+
+	rt.system.inc_nonce(&alice);
+	let _res = rt
+		.balances
+		.transfer(alice.clone(), charlie.clone(), 20)
+		.map_err(|e| println!("error: {}", e));
+
+	println!("{:#?}", rt);
 }
